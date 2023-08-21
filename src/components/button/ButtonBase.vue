1608b01cd542f9a447b7e0c63bb797c6de3bb9da
VJ Chrisintha <vionijannet@gmail.com>
2023-08-21 21:30:09 +0700

    Create component button & input text

---
+++ src/components/button/ButtonBase.vue
@@ -0,0 +1,9 @@
+<template>
+    <button class="w-full rounded-lg font-semibold text-xl bg-blue-600 text-white p-3 hover:bg-blue-800 transition duration-300" @click="$emit('click')">
+        <slot />
+    </button>
+</template>
+
+<script setup lang="ts">
+defineProps(["click"]);
+</script>
---
+++ src/components/input/InputText.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="w-full">
+        <label :for="labelFor" class="font-semibold text-lg">{{ labelText }}</label><br v-if="labelText" />
+        <input type="text" :name="labelFor" class="bg-gray-200 w-full p-4 rounded-lg text-lg text-gray-700 outline-0" :placeholder="placeholder" />
+    </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps(["labelFor", "placeholder", "labelText"]);
+</script>
