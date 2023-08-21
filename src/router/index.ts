a6823a006d032a80b5d2cbb770e0e520b2135de0
VJ Chrisintha <vionijannet@gmail.com>
2023-08-21 21:30:23 +0700

    Create login page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,33 +1,38 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'base',
+      path: "/",
+      name: "base",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/BaseView.vue'),
+      component: () => import("../views/BaseView.vue"),
       children: [
         {
-          path: '/',
-          name: 'Dashboard',
+          path: "/",
+          name: "Dashboard",
           // route level code-splitting
           // this generates a separate chunk (About.[hash].js) for this route
           // which is lazy-loaded when the route is visited.
-          component: () => import('../views/dashboard/DashboardPage.vue'),
+          component: () => import("../views/dashboard/DashboardPage.vue"),
         },
         {
-          path: '/campaign',
-          name: 'Campaign',
+          path: "/campaign",
+          name: "Campaign",
           // route level code-splitting
           // this generates a separate chunk (About.[hash].js) for this route
           // which is lazy-loaded when the route is visited.
-          component: () => import('../views/campaign/CampaignPage.vue'),
+          component: () => import("../views/campaign/CampaignPage.vue"),
         }
       ]
+    },
+    {
+      path: "/sign-in",
+      name: "login",
+      component: () => import("../views/LoginView.vue")
     }
   ]
 })
---
+++ src/views/LoginView.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="grid lg:grid-cols-2 h-screen">
+        <div class="hidden lg:block bg-blue-300">
+        </div>
+        <div class="bg-gray-200 flex items-center">
+            <div class="mx-auto bg-white rounded-2xl p-8 leading-8 w-3/4 max-w-xl">
+                <h1 class="font-bold text-2xl">Sign In to Your Account</h1>
+                <h2 class="text-gray-500">Enter email, password & token to login</h2>
+
+                <div class="py-8 space-y-4">
+                    <InputText label-for="email" label-text="Email Address" placeholder="example@mail.com" />
+                    <InputText label-for="password" label-text="Password" placeholder="********" />
+                    <InputText label-for="token" label-text="Token" placeholder="ex. 2T8Y67K" />
+                </div>
+
+                <ButtonBase @click="$router.push('/')">Sign In</ButtonBase>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import InputText from "@/components/input/InputText.vue";
+import ButtonBase from "@/components/button/ButtonBase.vue";
+</script>
