239ec0a991e7af98b98c8ac1a32ce1918ad2e8fd
VJ Chrisintha <vionijannet@gmail.com>
2023-08-26 01:30:03 +0700

    Change button font size

--- src/components/button/ButtonBase.vue
+++ src/components/button/ButtonBase.vue
@@ -1,5 +1,5 @@
 <template>
-    <button class="w-full rounded-lg font-semibold text-xl bg-blue-600 text-white p-3 hover:bg-blue-800 transition duration-300" @click="$emit('click')">
+    <button class="w-full rounded-lg font-semibold text-base bg-blue-600 text-white p-3 hover:bg-blue-800 transition duration-300" @click="$emit('click')">
         <slot />
     </button>
 </template>
