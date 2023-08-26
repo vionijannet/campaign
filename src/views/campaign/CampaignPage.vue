1b5d7dce84775326c3a7a864c47342691d916319
VJ Chrisintha <vionijannet@gmail.com>
2023-08-27 01:44:28 +0700

    Add routing to create from campaign page

--- src/views/campaign/CampaignPage.vue
+++ src/views/campaign/CampaignPage.vue
@@ -1,3 +1,18 @@
 <template>
-    <div>Campaign Page</div>
+    <div class="absolute top-0 right-0 my-6 mx-8 flex space-x-4">
+        <ButtonBase class="flex" @click="openPopupCreate">
+            <img src="../../assets/icon-create.svg" alt="Create">
+            <span class="ml-2">Create</span>
+        </ButtonBase>
+    </div>
 </template>
+
+<script setup lang="ts">
+import ButtonBase from '@/components/button/ButtonBase.vue';
+import router from "@/router";
+
+function openPopupCreate(): void {
+    console.log("harusnya buka pop up");
+    router.push("/campaign/create");
+}
+</script>
