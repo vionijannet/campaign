212eed4cd4061a04b8701b8e9caddcdcf3adc047
VJ Chrisintha <vionijannet@gmail.com>
2023-08-27 01:44:16 +0700

    Create layout create campaign page

--- src/router/index.ts
+++ src/router/index.ts
@@ -28,6 +28,14 @@ const router = createRouter({
           component: () => import("../views/campaign/CampaignPage.vue"),
         },
         {
+          path: "/campaign/create",
+          name: "Create Campaign",
+          // route level code-splitting
+          // this generates a separate chunk (About.[hash].js) for this route
+          // which is lazy-loaded when the route is visited.
+          component: () => import("../views/campaign/CreateCampaignPage.vue"),
+        },
+        {
           path: "/message",
           name: "Message",
           // route level code-splitting
---
+++ src/views/campaign/CreateCampaignPage.vue
@@ -0,0 +1,104 @@
+<template>
+    <div class="absolute top-0 right-0 my-6 mx-8 flex space-x-4">
+        <ButtonBase class="flex" @click="backToList">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
+            </svg>
+            <span class="ml-2">Back</span>
+        </ButtonBase>
+    </div>
+    <div class="bg-white w-full rounded-2xl p-6 space-y-6">
+        <InputText label-for="page-name" label-text="Page Name" placeholder="Page Name Goes Here"></InputText>
+        <InputText label-for="campaign-name" label-text="Campaign Name" placeholder="Type your campaign name"></InputText>
+        <div>
+            <p class="font-semibold text-lg">Scheduler</p>
+            <div class="flex justify-between items-center">
+                <p>Turning on to set schedule to your campaign</p>
+                
+                <label for="scheduler" tabindex="0" :aria-checked="isSchedulerOn" class="relative inline-flex flex-shrink-0 cursor-pointer transition-colors ease-in-out duration-200 border-2 border-transparent focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50 rounded-full" :class="{'bg-blue-primary': isSchedulerOn, 'bg-gray-100': !isSchedulerOn}">
+                    <input type="checkbox" id="scheduler" class="hidden" v-model="isSchedulerOn" />
+                    <span aria-hidden="true" class="rounded-full w-5 h-5 flex items-center justify-center text-gray-400 text-xs"></span>
+                    <span aria-hidden="true" class="rounded-full w-5 h-5 flex items-center justify-center text-gray-400 text-xs"></span>
+                    <span aria-hidden="true" class="absolute transform transition ease-in-out duration-200 h-5 w-5 rounded-full bg-white shadow flex items-center justify-center text-xs" :class="{'translate-x-full text-blue-primary': isSchedulerOn, 'translate-x-0 text-gray-400': !isSchedulerOn}"></span>
+                </label>
+            </div>
+            <div class="flex justify-between space-x-4 my-2">
+                <div class="bg-gray-100 flex items-center rounded-lg border w-full">
+                    <span class="mx-4">Date</span>
+                    <DatePicker class="!w-full !border-none"></DatePicker>
+                </div>
+                <div class="bg-gray-100 flex items-center rounded-lg border w-full">
+                    <span class="mx-4">Time</span>
+                    <DatePicker class="!w-full !border-none">
+                        <template #icon-calendar>
+                            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M5.665 1.5C3.135 1.5 1.5 3.233 1.5 5.916V14.084C1.5 16.767 3.135 18.5 5.665 18.5H14.333C16.864 18.5 18.5 16.767 18.5 14.084V5.916C18.5 3.233 16.865 1.5 14.334 1.5H5.665ZM14.333 20H5.665C2.276 20 0 17.622 0 14.084V5.916C0 2.378 2.276 0 5.665 0H14.334C17.723 0 20 2.378 20 5.916V14.084C20 17.622 17.723 20 14.333 20Z" fill="currentColor"/>
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M13.39 12.7675C13.259 12.7675 13.127 12.7335 13.006 12.6625L9.61502 10.6395C9.38902 10.5035 9.24902 10.2585 9.24902 9.99554V5.63354C9.24902 5.21954 9.58502 4.88354 9.99902 4.88354C10.413 4.88354 10.749 5.21954 10.749 5.63354V9.56954L13.775 11.3725C14.13 11.5855 14.247 12.0455 14.035 12.4015C13.894 12.6365 13.645 12.7675 13.39 12.7675Z" fill="currentColor"/>
+                            </svg>
+                        </template>
+                    </DatePicker>
+                </div>
+            </div>
+        </div>
+        <div>
+            <p class="font-semibold text-lg">Interval</p>
+            <div class="flex justify-between space-x-4 my-2">
+                <div class="bg-gray-100 flex items-center rounded-lg border w-full">
+                    <span class="mx-4">Minimal</span>
+                    <input type="text" class="relative inline-block w-full rounded-lg h-12 rounded-l-none outline-none py-[6px] px-[30px] pl-[10px]">
+                </div>
+                <div class="bg-gray-100 flex items-center rounded-lg border w-full">
+                    <span class="mx-4">Maximal</span>
+                    <input type="text" class="relative inline-block w-full rounded-lg h-12 rounded-l-none outline-none py-[6px] px-[30px] pl-[10px]">
+                </div>
+            </div>
+        </div>
+        <InputText label-for="audiens" label-text="Audiens" placeholder="Select audiens"></InputText>
+        <div>
+            <p class="font-semibold text-lg">Message</p>
+            <div class="w-full border rounded-lg p-4 my-2 space-y-1">
+                <div class="flex items-center">
+                    <input type="text" placeholder="Select image" disabled class="w-full h-12 border border-r-none rounded-r-none rounded-lg outline-none p-4" />
+                    <label for="upload-image" class="flex items-center cursor-pointer border border-blue-primary text-blue-primary rounded-lg h-12 p-2 px-4 rounded-l-none">Browse</label>
+                    <input type="file" name="image" id="upload-image" class="hidden">
+                </div>
+                <textarea rows="5" class="w-full outline-none border rounded-lg p-4" placeholder="Type your message"></textarea>
+            </div>
+            <ButtonBase class="my-2 !w-auto">Use Template</ButtonBase>
+        </div>
+        <hr />
+        <div class="w-full flex justify-end items-center space-x-2">
+            <ButtonBase type="error" class="!w-32" @click="backToList">Cancel</ButtonBase>
+            <ButtonBase class="!w-32">Save</ButtonBase>
+        </div>
+    </div>
+
+</template>
+
+<script setup lang="ts">
+import ButtonBase from '@/components/button/ButtonBase.vue';
+import InputText from '@/components/input/InputText.vue';
+import router from '@/router';
+import { ref } from 'vue';
+import DatePicker from 'vue-datepicker-next';
+
+const isSchedulerOn = ref(false);
+
+function backToList(): void {
+    router.push("/campaign");
+}
+</script>
+
+<style>
+.mx-input {
+    border: none;
+    border-radius: 0.5rem;
+    font-size: 1rem/* 16px */;
+    line-height: 1.5rem/* 24px */;
+    border-top-left-radius: 0px;
+    border-bottom-left-radius: 0px;
+    box-shadow: none;
+    height: 3rem/* 48px */;
+}
+</style>
