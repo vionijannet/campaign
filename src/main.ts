bae65324975b64cc224533d1e96f225d545ab7e7
VJ Chrisintha <vionijannet@gmail.com>
2023-08-27 01:43:39 +0700

    Install datepicker

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import "./assets/style.css"
+import "vue-datepicker-next/index.css"
 
 import { createApp } from "vue"
+import DatePicker from "vue-datepicker-next"
 import { createPinia } from "pinia"
 
 import App from "./App.vue"
@@ -10,5 +12,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(DatePicker)
 
 app.mount("#app")
