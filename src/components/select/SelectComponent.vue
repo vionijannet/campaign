e47fe9b625603ed8b9b46b0f9106bb0b021013bf
VJ Chrisintha <vionijannet@gmail.com>
2023-09-10 21:57:02 +0700

    Fix bind value in select component

--- src/components/select/SelectComponent.vue
+++ src/components/select/SelectComponent.vue
@@ -1,14 +1,17 @@
 <template>
-    <select class="outline-none" @change="setData">
-        <option v-for="op in optionList" :value="op" :selected="op.key === selected">{{ op.value }}</option>
+    <select class="outline-none" @change="setData($event)">
+        <option v-for="op in optionList" :value="op.key" :selected="op.key === selected">{{ op.value }}</option>
     </select>
 </template>
 
 <script setup lang="ts">
+import { OptionEntity } from '../ComponentEntity';
+
 const props = defineProps(["selected", "optionList"])
 const emit = defineEmits(["change"])
 
 function setData(event: Event): void {
-    emit("change", (event.target as HTMLSelectElement).value);
+    const selectedOption = props.optionList.find((op: OptionEntity) => op.key === (event.target as HTMLSelectElement).value);
+    emit("change", selectedOption);
 }
 </script>
