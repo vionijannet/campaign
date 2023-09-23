a5136eba48d654b7e6b453392d6fbfaa8b5109eb
VJ Chrisintha <vionijannet@gmail.com>
2023-09-24 02:04:01 +0700

    Enhance recaptcha

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -12,25 +12,34 @@
                     <InputText label-for="email" label-text="Email Address" placeholder="example@mail.com" />
                     <div>
                         <InputText label-for="password" label-text="Password" placeholder="********" />
-                        <p class="text-sm underline cursor-pointer py-1" @click="isPopupForgotPasswordShown = true">Forgot your password?</p>
+                        <p class="text-sm underline cursor-pointer py-1" @click="isPopupForgotPasswordShown = true">
+                            Forgot your password?
+                        </p>
                     </div>
                 </div>
 
-                <div class="flex items-center justify-center">
-                    <vue-recaptcha :sitekey="siteKey" class="pb-2 mx-auto"></vue-recaptcha>
+                <div class="flex items-center justify-center pb-2">
+                    <vue-recaptcha :sitekey="siteKey" :class="isRecaptchaError ? 'border border-red-500' : ''" class="mx-auto w-[304px] h-[78px]"
+                        @verify="recaptchaVerified" @expire="recaptchaExpired" @fail="recaptchaFailed" @error="recaptchaError" ref="recaptcha">
+                    </vue-recaptcha>
                 </div>
 
                 <div>
-                    <ButtonBase @click="$router.push('/')">Sign In</ButtonBase>
-                    <p class="text-xs text-right py-1 text-neutral-500">Not on RepliEm yet? <b class="underline cursor-pointer" @click="isPopupCreateAccountShown = true">Sign Up</b></p>
+                    <ButtonBase @click="signIn">Sign In</ButtonBase>
+                    <p class="text-xs text-right py-1 text-neutral-500">Not on RepliEm yet?
+                        <b class="underline cursor-pointer" @click="isPopupCreateAccountShown = true">Sign Up</b>
+                    </p>
                 </div>
 
-                <p class="text-xs text-neutral-500">by continuing, you agree to RepliEm's <b class="underline cursor-pointer" @click="redirectTo('tos')">Terms of Service</b> and acknowledge you've read our <b class="underline cursor-pointer" @click="redirectTo('policy')">Privacy Policy</b>.</p>
+                <p class="text-xs text-neutral-500">by continuing, you agree to RepliEm's
+                    <b class="underline cursor-pointer" @click="redirectTo('tos')">Terms of Service</b> and acknowledge
+                    you've read our <b class="underline cursor-pointer" @click="redirectTo('policy')">Privacy Policy</b>.
+                </p>
             </div>
         </div>
     </div>
 
-    <ModalComponent v-if="isPopupCreateAccountShown" @close="isPopupCreateAccountShown=false">
+    <ModalComponent v-if="isPopupCreateAccountShown" @close="isPopupCreateAccountShown = false">
         <CreateAccount></CreateAccount>
     </ModalComponent>
 </template>
@@ -38,7 +47,7 @@
 <script setup lang="ts">
 import InputText from "@/components/input/InputText.vue";
 import ButtonBase from "@/components/button/ButtonBase.vue";
-import { ref } from "vue";
+import { Ref, onMounted, ref } from "vue";
 import router from "@/router";
 import ModalComponent from "@/components/modal/ModalComponent.vue";
 import CreateAccount from "./account/CreateAccount.vue";
@@ -47,11 +56,34 @@ import vueRecaptcha from 'vue3-recaptcha2';
 const isPopupForgotPasswordShown = ref(false);
 const isPopupCreateAccountShown = ref(false);
 
+const recaptcha: Ref<any> = ref(null);
+const isRecaptchaError = ref(false);
+
 const siteKey = "6Lc9Z40hAAAAAFRdtS72GNcB76a1ltmdqWQ-6Zd5";
 
 function redirectTo(name: string): void {
     const path = name === "tos" ? "/terms" : "/policy";
     const routeData = router.resolve(path);
     window.open(routeData.href, "_blank");
+}
+
+function recaptchaVerified(response: any): void {
+    console.log("v", response);
+}
+
+function recaptchaExpired(): void {
+    recaptcha.value.reset();
+}
+
+function recaptchaFailed(): void {
+    console.log("f");
+}
+
+function recaptchaError(reason: any): void {
+    console.log("e", reason);
+}
+
+function signIn(): void {
+    router.push('/')
 }
 </script>
