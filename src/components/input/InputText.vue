40b37ff107bd212e25dc9ea91115448b4a564832
VJ Chrisintha <vionijannet@gmail.com>
2023-09-23 15:09:09 +0700

    Add prop and emit for seach and input text

--- src/components/input/InputText.vue
+++ src/components/input/InputText.vue
@@ -1,10 +1,22 @@
 <template>
     <div class="w-full space-y-1">
         <label :for="labelFor" class="font-semibold text-base" v-if="labelText">{{ labelText }}</label>
-        <input type="text" :disabled="disabled" :name="labelFor" :value="value" class="bg-gray-100 focus:bg-white focus:border focus:border-gray-200 w-full p-4 rounded-lg text-lg text-gray-700 outline-0" :placeholder="placeholder" />
+        <input type="text" :disabled="disabled" :name="labelFor" :value="text" class="bg-gray-100 focus:bg-white focus:border focus:border-gray-200 w-full p-4 rounded-lg text-lg text-gray-700 outline-0" :placeholder="placeholder" />
     </div>
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue';
+
 const props = defineProps(["labelFor", "placeholder", "labelText", "value", "disabled"]);
+const emit = defineEmits(["type"]);
+
+const text = computed({
+    get() {
+        return props.value;
+    },
+    set(newValue) {
+        emit("type", newValue);
+    }
+});
 </script>
--- src/components/search/Search.vue
+++ src/components/search/Search.vue
@@ -1,6 +1,29 @@
 <template>
     <div class="bg-white rounded-lg px-4 w-80 h-12 flex items-center justify-between">
-        <input type="text" class="outline-none w-64" placeholder="Search" />
+        <input type="text" v-model="searchValue" class="outline-none w-64"
+            :placeholder="placeholder" @keyup.enter="onEnter" @keypress="onType" />
         <img src="../../assets/search.svg" alt="Search">
     </div>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const props = defineProps({
+    placeholder: {
+        default: "Search"
+    }
+})
+
+const searchValue = ref("");
+
+const emit = defineEmits(["enter", "type"])
+
+function onEnter(): void {
+    emit("enter", searchValue.value)
+}
+
+function onType(): void {
+    emit("type", searchValue.value);
+}
+</script>
