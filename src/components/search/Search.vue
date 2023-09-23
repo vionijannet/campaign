cdb55b19069f32f1296a4c8a580aecdd5ba2a702
VJ Chrisintha <vionijannet@gmail.com>
2023-09-23 14:56:30 +0700

    Create component search

---
+++ src/components/search/Search.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="bg-white rounded-lg px-4 w-80 h-12 flex items-center justify-between">
+        <input type="text" class="outline-none w-64" placeholder="Search" />
+        <img src="../../assets/search.svg" alt="Search">
+    </div>
+</template>
