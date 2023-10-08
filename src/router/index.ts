1f09a338eb257878f2e4a6cef4042b077948a9fe
VJ Chrisintha <vionijannet@gmail.com>
2023-10-08 22:09:45 +0700

    Create audiens page

--- src/router/index.ts
+++ src/router/index.ts
@@ -44,6 +44,14 @@ const router = createRouter({
           component: () => import("../views/campaign/UpdateCampaignPage.vue"),
         },
         {
+          path: "/audiens",
+          name: "Audiens",
+          // route level code-splitting
+          // this generates a separate chunk (About.[hash].js) for this route
+          // which is lazy-loaded when the route is visited.
+          component: () => import("../views/audiens/AudiensPage.vue"),
+        },
+        {
           path: "/message",
           name: "Message",
           // route level code-splitting
