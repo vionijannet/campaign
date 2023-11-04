af1f0a084493c8bae08f7eda1c30009302794f99
VJ Chrisintha <vionijannet@gmail.com>
2023-11-04 15:09:57 +0700

    Add prop validation message

--- src/components/input/InputText.vue
+++ src/components/input/InputText.vue
@@ -1,16 +1,18 @@
 <template>
     <div class="w-full space-y-1">
         <label :for="labelFor" class="font-semibold text-base" v-if="labelText">{{ labelText }}</label>
-        <input type="text" :disabled="disabled" :name="labelFor" :placeholder="placeholder" v-model="text"
+        <input type="text" :disabled="disabled" :name="labelFor" :placeholder="placeholder" v-model="text" @blur="leave"
+            :class="{ 'border-red-500': validation && validation.length > 0 }"
             class="disabled:bg-gray-100 bg-white border border-gray-100 w-full p-4 rounded-lg text-lg text-gray-700 outline-0" />
+        <p class="text-sm text-red-500" v-if="validation && validation.length > 0">{{ validation }}</p>
     </div>
 </template>
 
 <script setup lang="ts">
 import { computed } from 'vue';
 
-const props = defineProps(["labelFor", "placeholder", "labelText", "value", "disabled"]);
-const emit = defineEmits(["type"]);
+const props = defineProps(["labelFor", "placeholder", "labelText", "value", "disabled", "validation"]);
+const emit = defineEmits(["type", "leave"]);
 
 const text = computed({
     get() {
@@ -20,4 +22,8 @@ const text = computed({
         emit("type", newValue);
     }
 });
+
+function leave(): void {
+    emit("leave");
+}
 </script>
