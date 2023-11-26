ccc299b8f54ec85f1508594d024be5d0a74ae376
Vioni <vionijannet@gmail.com>
2023-11-26 20:19:45 +0700

    Create function to generate file checksum

--- src/util/TextFormatter.ts
+++ src/util/TextFormatter.ts
@@ -1,3 +1,8 @@
+import { Observable } from "rxjs";
+import { MD5 } from "crypto-js";
+import * as CryptoJS from "crypto-js";
+import { map } from "rxjs/operators";
+
 export class TextFormatter {
     public static convertEmptyPropertyToNull(obj: any): any {
         for (const key in obj) {
@@ -18,5 +23,46 @@ export class TextFormatter {
         }
         
         return obj;
+    }
+
+    public static generateFileChecksum(file: File): Observable<string> {
+        const generator = new FileChecksumGenerator();
+        return generator.execute(file);
+    }
+}
+
+class FileChecksumGenerator {
+    execute(file: File): Observable<string> {
+        return this.readFileAndCalculateHash(file)
+            .pipe(
+                map((checksum) => checksum.toUpperCase())
+            )
+    }
+
+    private readFileAndCalculateHash(blob: Blob): Observable<string> {
+        return new Observable((observer) => {
+            if (!(blob instanceof Blob)) {
+                observer.error(
+                    new Error("Failed to generate checksum due to invalid file")
+                );
+                return;
+            }
+
+            const reader = new FileReader();
+
+            reader.onerror = (e) => observer.error(e);
+            reader.onload = () => {
+                observer.next(this.calculateMd5(reader.result as string));
+            }
+            reader.onloadend = () => observer.complete();
+
+            reader.readAsBinaryString(blob);
+
+            return () => reader.abort();
+        })
+    }
+
+    private calculateMd5(filestring: string): string {
+        return MD5(CryptoJS.enc.Latin1.parse(filestring)).toString();
     }
 }
