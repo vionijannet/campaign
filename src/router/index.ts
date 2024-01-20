bad2b58142d23a84d464b817269fed3231fa916c
Vioni <vionijannet@gmail.com>
2024-01-20 21:59:32 +0700

    Change path for audience group

--- src/router/index.ts
+++ src/router/index.ts
@@ -57,7 +57,7 @@ const router = createRouter({
           // route level code-splitting
           // this generates a separate chunk (About.[hash].js) for this route
           // which is lazy-loaded when the route is visited.
-          component: () => import("@/views/group/GroupPage.vue"),
+          component: () => import("@/views/audience/AudienceGroupPage.vue"),
         },
         {
           path: "/message",
