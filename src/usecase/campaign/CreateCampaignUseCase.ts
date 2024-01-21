3faa9ee5c0cb2f42cd27cc0102fc55d217bc3add
Vioni <vionijannet@gmail.com>
2024-01-21 20:15:34 +0700

    Add validation in campaign

--- src/usecase/campaign/CreateCampaignUseCase.ts
+++ src/usecase/campaign/CreateCampaignUseCase.ts
@@ -5,19 +5,83 @@ import { CampaignService } from "@/service/CampaignService";
 import { Observable } from "rxjs";
 
 export interface CreateCampaignUseCase {
-    validate(template: CreateCampaignReq): FieldError[];
-    execute(template: CreateCampaignReq): Observable<BaseResp>
+    validate(campaign: CreateCampaignReq): FieldError[];
+    execute(campaign: CreateCampaignReq): Observable<BaseResp>
 }
 
 export class CreateCampaignUseCaseImpl implements CreateCampaignUseCase {
     constructor(private campaignService: CampaignService) {}
 
-    validate(template: CreateCampaignReq): FieldError[] {
+    validate(campaign: CreateCampaignReq): FieldError[] {
         const error: FieldError[] = [];
+
+        if (campaign.campaign_name.trim().length < 1) {
+            error.push({
+                field: "campaign_name",
+                message: ["Required"]
+            });
+        }
+
+        if (campaign.is_scheduled) {
+            if (campaign.scheduled_date.trim().length < 1) {
+                error.push({
+                    field: "scheduled_date",
+                    message: ["Required"]
+                },{
+                    field: "scheduled_time",
+                    message: ["Required"]
+                });
+            } else {
+                const dateTime = campaign.scheduled_date.trim().split(" ");
+                const date = dateTime[0];
+                const time = dateTime[1];
+
+                if (date.trim().length < 1) {
+                    error.push({
+                        field: "scheduled_date",
+                        message: ["Required"]
+                    });
+                } else if (time.trim().length < 1) {
+                    error.push({
+                        field: "scheduled_time",
+                        message: ["Required"]
+                    });
+                }
+            }
+        }
+
+        if (campaign.interval_min < 1) {
+            error.push({
+                field: "interval_min",
+                message: ["Required"]
+            });
+        }
+
+        if (campaign.interval_max < 1) {
+            error.push({
+                field: "interval_max",
+                message: ["Required"]
+            });
+        }
+
+        if (campaign.audience_list.length < 1) {
+            error.push({
+                field: "audience_list",
+                message: ["Required"]
+            });
+        }
+
+        if (campaign.template_id.length < 1 && campaign.message_list.length < 1) {
+            error.push({
+                field: "template_id",
+                message: ["Required"]
+            });
+        }
+
         return error;
     }
 
-    execute(template: CreateCampaignReq): Observable<BaseResp> {
-        return this.campaignService.createCampaign({...template});
+    execute(campaign: CreateCampaignReq): Observable<BaseResp> {
+        return this.campaignService.createCampaign({...campaign});
     }
 }
--- src/usecase/campaign/UpdateCampaignUseCase.ts
+++ src/usecase/campaign/UpdateCampaignUseCase.ts
@@ -5,19 +5,83 @@ import { CampaignService } from "@/service/CampaignService";
 import { Observable } from "rxjs";
 
 export interface UpdateCampaignUseCase {
-    validate(template: UpdateCampaignReq): FieldError[];
-    execute(template: UpdateCampaignReq): Observable<BaseResp>
+    validate(campaign: UpdateCampaignReq): FieldError[];
+    execute(campaign: UpdateCampaignReq): Observable<BaseResp>
 }
 
 export class UpdateCampaignUseCaseImpl implements UpdateCampaignUseCase {
     constructor(private campaignService: CampaignService) {}
 
-    validate(template: UpdateCampaignReq): FieldError[] {
+    validate(campaign: UpdateCampaignReq): FieldError[] {
         const error: FieldError[] = [];
+
+        if (campaign.campaign_name.trim().length < 1) {
+            error.push({
+                field: "campaign_name",
+                message: ["Required"]
+            });
+        }
+
+        if (campaign.is_scheduled) {
+            if (campaign.scheduled_date.trim().length < 1) {
+                error.push({
+                    field: "scheduled_date",
+                    message: ["Required"]
+                },{
+                    field: "scheduled_time",
+                    message: ["Required"]
+                });
+            } else {
+                const dateTime = campaign.scheduled_date.trim().split(" ");
+                const date = dateTime[0];
+                const time = dateTime[1];
+
+                if (date.trim().length < 1) {
+                    error.push({
+                        field: "scheduled_date",
+                        message: ["Required"]
+                    });
+                } else if (time.trim().length < 1) {
+                    error.push({
+                        field: "scheduled_time",
+                        message: ["Required"]
+                    });
+                }
+            }
+        }
+
+        if (campaign.interval_min < 1) {
+            error.push({
+                field: "interval_min",
+                message: ["Required"]
+            });
+        }
+
+        if (campaign.interval_max < 1) {
+            error.push({
+                field: "interval_max",
+                message: ["Required"]
+            });
+        }
+
+        if (campaign.audience_list.length < 1) {
+            error.push({
+                field: "audience_list",
+                message: ["Required"]
+            });
+        }
+
+        if (campaign.template_id.length < 1 && campaign.message_list.length < 1) {
+            error.push({
+                field: "template_id",
+                message: ["Required"]
+            });
+        }
+
         return error;
     }
 
-    execute(template: UpdateCampaignReq): Observable<BaseResp> {
-        return this.campaignService.updateCampaign({...template});
+    execute(campaign: UpdateCampaignReq): Observable<BaseResp> {
+        return this.campaignService.updateCampaign({...campaign});
     }
 }
