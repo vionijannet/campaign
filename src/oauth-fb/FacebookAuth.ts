35d2ae7283935d80c7bb96d93b2f2c40e1e6452c
Vioni <vionijannet@gmail.com>
2024-03-25 21:43:19 +0700

    Fix apapun itulah

--- src/oauth-fb/FacebookAuth.ts
+++ src/oauth-fb/FacebookAuth.ts
@@ -41,8 +41,8 @@ export async function initFacebook(appId: string | undefined): Promise<void> {
             var js, fjs = d.getElementsByTagName(s)[0];
             if (d.getElementById(id)) { return; }
             js = d.createElement(s); js.id = id;
-            js.src = "https://connect.facebook.net/en_US/sdk.js";
-            fjs.parentNode.insertBefore(js, fjs);
+            (js as any).src = "https://connect.facebook.net/en_US/sdk.js";
+            (fjs.parentNode as ParentNode).insertBefore(js, fjs);
         } (document, 'script', 'facebook-jssdk'));    
     })
 }
