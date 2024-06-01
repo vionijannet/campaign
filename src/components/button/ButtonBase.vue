f513354ea1a9be75b417fba67b8f45cf7f4f2304
Vioni <vionijannet@gmail.com>
2024-06-01 22:12:51 +0700

    Add props disabled in button

--- src/components/button/ButtonBase.vue
+++ src/components/button/ButtonBase.vue
@@ -1,11 +1,12 @@
 <template>
     <button class="w-full border rounded-lg font-semibold text-base p-3 transition duration-300"
         :class="{
+            'border-gray-400 bg-gray-400 text-white hover:bg-gray-400 cursor-not-allowed': disabled,
             'border-red-500 bg-white text-red-500 hover:bg-gray-100': type === 'error',
             'border-blue-600 bg-blue-600 text-white hover:bg-blue-800': type === 'primary',
             'border-blue-600 bg-white text-blue-600 hover:bg-gray-100': type === 'secondary',
         }"
-        @click="click"> 
+        @click="click" :disabled="disabled"> 
         <slot />
     </button>
 </template>
@@ -14,6 +15,10 @@
 const props = defineProps({
     type: {
         default: "primary"
+    },
+    disabled: {
+        default: false,
+        type: Boolean,
     }
 });
 
