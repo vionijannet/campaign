dc4c593adced5045509cade74208a9ef967424f9
Vioni <vionijannet@gmail.com>
2024-07-08 22:32:50 +0700

    Create app yaml

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,9 +10,10 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import("../views/BaseView.vue"),
+      redirect: "/dashboard",
       children: [
         {
-          path: "/",
+          path: "/dashboard",
           name: "Dashboard",
           // route level code-splitting
           // this generates a separate chunk (About.[hash].js) for this route
